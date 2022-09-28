8f4dec4e8ad5be6e1893365b6abeb40cc161a777
quocluongstp@gmail.com <quocluongstp@gmail.com>
2022-09-28 13:24:30 -0700

    Initial Commit

---
+++ LootLogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LootLogger
+//
+//  Created by Quoc Luong on 9/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
